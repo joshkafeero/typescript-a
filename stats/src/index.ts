997f92937413d85fbe2504298c76f01ff5914df8
Joshua Kafeero <joshkafeero@gmail.com>
2020-06-28 12:34:15 -0700

    Abstract class and generics refactor

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,9 +1,18 @@
-import fs from 'fs';
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8'
-}).split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
-console.log(matches);
+const reader = new MatchReader('football.csv');
+reader.read();
+const matches = reader.data;
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin){
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
