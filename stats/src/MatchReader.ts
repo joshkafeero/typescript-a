7bc918b17b0c2b7563269a5f4b56e71695374153
Joshua Kafeero <joshkafeero@gmail.com>
2020-06-29 17:10:49 -0700

    Analyzers and output targets

--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -1,7 +1,6 @@
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
+import { MatchData } from './MatchData';
 
 interface DataReader {
   read(): void;
@@ -9,18 +8,23 @@ interface DataReader {
 }
 
 export class MatchReader {
+  matches: MatchData[] = [];
   constructor(public reader: DataReader) { }
 
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6]
+        ]
+      });
+  }
 
-  // .map((row: string[]): MatchData => {
-  //   return [
-  //     dateStringToDate(row[0]),
-  //     row[1],
-  //     row[2],
-  //     parseInt(row[3]),
-  //     parseInt(row[4]),
-  //     row[5] as MatchResult,
-  //     row[6]
-  //   ]
-  // })
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,18 +1,22 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { Summary } from './Summary';
 
-const reader = new MatchReader('football.csv');
-reader.read();
-const matches = reader.data;
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv'); 
 
-let manUnitedWins = 0;
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface.
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
-for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin){
-    manUnitedWins++;
-  }
-}
+// const matches: MatchData[] = matchReader.matches;
 
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
+
+summary.buildAndPrintReport(matchReader.matches);
