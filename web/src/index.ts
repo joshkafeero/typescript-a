268f96f1c8223a060e69f52a0f68bb5580256373
Joshua Kafeero <joshkafeero@gmail.com>
2020-06-30 20:00:40 -0700

    Initial eventing done.

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,9 @@ import { User } from './model/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.set({ name: 'newName'});
+user.on('change', () => { console.log('Change #1')});
+user.on('change', () => { console.log('Change #2')}); 
+user.on('save', () => { console.log('Save performed')});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.trigger('change');
+user.trigger('save');
--- web/src/model/User.ts
+++ web/src/model/User.ts
@@ -4,7 +4,7 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => {}
+type Callback = () => void;
 
 export class User {
   events: { [key: string]: Callback[] } = {};
@@ -19,5 +19,22 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  on(eventName: string, callback: Callback) { }
+  on(eventName: string, callback: Callback): void { 
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(callback => {
+      callback();
+    });
+  }
+
 }
